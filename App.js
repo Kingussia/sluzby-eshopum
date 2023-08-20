2931676511f836589fe5cd5bcd34ae73e756a39f
René Schneer <brno_sklad@czstyle.cz>
2023-08-20 11:41:05 +0200

    Popis vašich změn

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import OrdersPage from './OrdersPage'; // Upravte cestu podle umístění souboru OrdersPage.js
+import React, { useEffect, useState } from 'react';
 
 function App() {
-    return (
-        <div className="App">
-            <OrdersPage />
-        </div>
-    );
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    // Načtení dat z API
+    fetch('/api/orders')
+      .then(response => response.json())
+      .then(data => setOrders(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: 'grey', height: '100vh', padding: '20px' }}>
+      <h1>Seznam objednávek</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+        <button>Tlačítko 1</button>
+        <button>Tlačítko 2</button>
+        <button>Tlačítko 3</button>
+        <button>Tlačítko 4</button>
+      </div>
+      <div>
+        {orders.map(order => (
+          <div key={order.number} style={{ marginBottom: '10px' }}>
+            <input type="checkbox" />
+            <button style={{ marginLeft: '10px' }}>
+              {order.number} - {order.status}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
