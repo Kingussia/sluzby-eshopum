4db46743a5933c9b4b2ba68182c3de9dfb6e44e3
René Schneer <brno_sklad@czstyle.cz>
2023-08-20 10:46:30 +0200

    Popis vašich změn

--- server.js
+++ server.js
@@ -6,20 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // Povolení CORS pro všechny domény, abyste mohli volat API z React aplikace
 
-// Předpokládáme, že máte nastavené připojení k databázi
-// TODO: Zde vložte kód pro připojení k databázi
-
 app.get('/api/orders', (req, res) => {
     const sql = 'SELECT * FROM c5.pneutyres.SE_SalesOrder'; // Nahraďte "your_table_name" názvem vaší tabulky
     db.query(sql, (err, results) => {
         if (err) {
-            res.status(500).send('Server error');
+            res.status(500).send(`Server error: ${err.message}`);
             return;
         }
         res.json(results);
     });
 });
-
 
 app.get('/', (req, res) => {
     res.send('API is working');
