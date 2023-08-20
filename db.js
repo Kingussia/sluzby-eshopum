a31a22261412b6ccb995927c8bcea86c18c3a6b7
René Schneer <brno_sklad@czstyle.cz>
2023-08-20 10:28:45 +0200

    Popis vašich změn

--- db.js
+++ db.js
@@ -11,5 +11,9 @@ db.connect((err) => {
   if (err) throw err;
   console.log('Connected to the database');
 });
+db.query('SELECT 1 + 1 AS solution', (err, results) => {
+    if (err) throw err;
+    console.log('The solution is: ', results[0].solution);
+});
 
 module.exports = db;
